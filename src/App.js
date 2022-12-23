8bc87587b516bdf275a6da1ff2817d1a17450660
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2022-12-23 10:26:36 +0200

    added notifications

--- src/App.js
+++ src/App.js
@@ -3,23 +3,31 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 import FormInput from "./Components/FormInput";
 import QAList from "./Components/QAList";
 import { questions } from "./data";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
+  const notify = (msg) =>
+    toast.success(msg, {
+      position: toast.POSITION.TOP_LEFT,
+    });
   const [Data, setData] = useState(questions);
   const localData = JSON.parse(localStorage.getItem("items"));
   const AddItem = () => {
     localStorage.setItem("items", JSON.stringify([...questions]));
     setData([...questions]);
+    notify("Question added successfully");
   };
 
   const DeleteAllItems = () => {
     localStorage.removeItem("items");
     setData([]);
     questions.splice(0, questions.length);
+    setTimeout(() => notify("All questions removed successfully"), 300);
   };
   const DeleteOneItem = (newData) => {
     localStorage.setItem("items", JSON.stringify([...newData]));
     setData([...newData]);
-    console.log(localData);
+    notify("Question removed successfully");
     if (localData.length <= 1) {
       DeleteAllItems();
     }
@@ -50,6 +58,7 @@ function App() {
             )}
           </Col>
         </Row>
+        <ToastContainer />
       </Container>
     </div>
   );
